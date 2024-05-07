814aef36532ed3308b63b0ca93a0d7b624afdf8b
cipher <laariachris@gmail.com>
2024-05-07 15:47:54 +0300

    " removed Images/Testimonial1 and Images/Testimonial2"
    This commit message accurately describes the code diff by stating that two files, Images/Testimonial1 and Images/Testimonial2, were removed. It

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-/*============= Sticky navbar ============*/
-
+/*============= scroll Section active linkr ============*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
+   sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop + 150; // Corrected the calculation here
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+        navlinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        })
+    }
+   })
+}
+
+    /*============= Sticky navbar ============*/
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
-}
+
+/*============= Swiper ============*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
